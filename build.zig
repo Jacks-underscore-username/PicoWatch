8fce894a46f3e7257916300a4e11c8244b7781d7
Jacks-underscore-username <jacksunderscoreusername@gmail.com>
2026-01-23 09:52:37 -0600

    Made the min C code to run screen, and have zig build code to add all C files

--- build.zig
+++ build.zig
@@ -1,11 +1,13 @@
 const std = @import("std");
 const microzig = @import("microzig");
+const eql = std.mem.eql;
+const ArrayList = std.ArrayList;
 
 const MicroBuild = microzig.MicroBuild(.{
     .rp2xxx = true,
 });
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const mz_dep = b.dependency("microzig", .{});
     const mb = MicroBuild.init(b, mz_dep) orelse return;
 
@@ -15,6 +17,33 @@ pub fn build(b: *std.Build) void {
         .optimize = .Debug,
         .root_source_file = b.path("src/main.zig"),
     });
+
+    const allocator = b.allocator;
+
+    var rootDir = try std.fs.cwd().openDir(".", .{ .iterate = true });
+    defer rootDir.close();
+
+    var dirsToScan: ArrayList([]const u8) = .empty;
+    defer dirsToScan.deinit(allocator);
+
+    try dirsToScan.append(allocator, "lib");
+
+    while (dirsToScan.pop()) |dirName| {
+        var dir = try rootDir.openDir(dirName, .{ .iterate = true });
+        var iterator = dir.iterate();
+        while (try iterator.next()) |item| {
+            const path = b.pathJoin(&.{ dirName, item.name });
+            if (item.kind == .directory) {
+                firmware.app_mod.addIncludePath(b.path(path));
+                try dirsToScan.append(allocator, path);
+            } else if (item.kind == .file and std.mem.eql(u8, std.fs.path.extension(item.name), ".c")) {
+                std.debug.print("Added C source file: {s}\n", .{path});
+                firmware.app_mod.addCSourceFile(.{ .file = b.path(path) });
+            }
+        }
+    }
+
+    firmware.app_mod.addIncludePath(b.path("lib"));
 
     firmware.app_mod.addIncludePath(b.path("c_lib"));
     firmware.app_mod.addCSourceFile(.{ .file = b.path("c_lib/arithmetic.c") });
--- main.c
+++ main.c
@@ -1,8 +1,53 @@
-#include "LCD_test.h"
+#include "DEV_Config.h"
 
-int main(void)
-{
-    AMOLED_1IN8_LVGL_Test();
+#include "AMOLED_1in8.h"
+// #include "FT3168.h"
+// #include "LCD_test.h"
+#include "AMOLED_1in8.h"
+// #include "FT3168.h"
+// #include "LVGL_example.h"
+// #include "PCF85063A.h"
+// #include "QMI8658.h"
+// #include "lvgl.h"
+#include "pico/stdlib.h"
+#include "qspi_pio.h"
+#include <stdio.h>
 
-    return 0;
+int main(void) {
+  if (DEV_Module_Init() != 0) {
+    return -1;
+  }
+
+  printf("AMOLED_1IN8_LCGL_test Demo\r\n");
+  /*QSPI PIO Init*/
+  QSPI_GPIO_Init(qspi);
+  QSPI_PIO_Init(qspi);
+  QSPI_4Wrie_Mode(&qspi);
+  /*Init LCD*/
+  AMOLED_1IN8_Init();
+  AMOLED_1IN8_SetBrightness(100);
+  AMOLED_1IN8_Clear(WHITE);
+  /*Init touch screen*/
+  //   FT3168_Init(FT3168_Point_Mode);
+  /*Init RTC*/
+  // PCF85063A_Init();
+  /*Init IMU*/
+  // QMI8658_init();
+  /*Init LVGL*/
+  // LVGL_Init();
+  // Widgets_Init();
+
+  uint64_t c = 0;
+
+  while (1) {
+    AMOLED_1IN8_Clear(c);
+
+    c++;
+
+    //   lv_task_handler();
+    DEV_Delay_ms(5);
+  }
+
+  DEV_Module_Exit();
+  return 0;
 }
